b2260828349ea3fbdc9ef8205a461d980d166587
TetyanaPrymak <tetyanaprymak17@gmail.com>
2022-11-23 12:56:18 +0200

    Updated methods names

--- index.rb
+++ index.rb
@@ -2,10 +2,13 @@ require 'yaml'
 require 'date'
 
 FILE_PATH = 'cars.yml'.freeze
+BY_PRICE = 'price'.freeze
+BY_DATE_ADDED = 'date_added'.freeze
+ALLOWED_SORT_OPTIONS = [BY_PRICE, BY_DATE_ADDED].freeze
+RULES_LIST = ["make", "model", "year_from", "year_to", "price_from", "price_to"].freeze
 cars = YAML.load(File.read(FILE_PATH))
 filter_list = {}
 filter_result = []
-RULES_LIST = ["make", "model", "year_from", "year_to", "price_from", "price_to"].freeze
 
 puts "Please select search rules."
 RULES_LIST.each do |rule|
@@ -23,27 +26,24 @@ def equal?(user_input, db_value)
   user_input.empty? || user_input.casecmp?(db_value)
 end
 
-def between_then?(user_input_from, user_input_to, db_value)
-  (user_input_from.empty? || user_input_from.to_i <= db_value)&&
+def between?(user_input_from, user_input_to, db_value)
+  (user_input_from.empty? || user_input_from.to_i <= db_value) &&
   (user_input_to.empty? || user_input_to.to_i >= db_value)
 end
 
-def conditions_ok?(filter_list, car)
+def car_matches?(filter_list, car)
+  equal?(filter_list["make"], car["make"]) &&
   equal?(filter_list["model"], car["model"]) &&
-  between_then?(filter_list["year_from"], filter_list["year_to"], car["year"]) &&
-  between_then?(filter_list["price_from"], filter_list["price_to"], car["price"])
+  between?(filter_list["year_from"], filter_list["year_to"], car["year"]) &&
+  between?(filter_list["price_from"], filter_list["price_to"], car["price"])
 end
 
 cars.each do |car|
-  if conditions_ok?(filter_list, car)
-    car["date_added"] = Date.strptime(car["date_added"], '%d/%m/%y')
-    filter_result << car
-  end
+  next unless car_matches?(filter_list, car)
+  car["date_added"] = Date.strptime(car["date_added"], '%d/%m/%y')
+  filter_result << car
 end
 
-BY_PRICE = 'price'.freeze
-BY_DATE_ADDED = 'date_added'.freeze
-ALLOWED_SORT_OPTIONS = [BY_PRICE, BY_DATE_ADDED].freeze
 sort_option = ALLOWED_SORT_OPTIONS.include?(sort_option) ? sort_option : BY_DATE_ADDED
 
 if sort_direction == "asc"
