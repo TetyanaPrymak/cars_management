ebea2a3b84b2a4464bb6ddb276a403b699637faf
TetyanaPrymak <tetyanaprymak17@gmail.com>
2023-01-04 22:04:38 +0200

    Combined two similar methods according to the review remarks

--- Rakefile.rb
+++ Rakefile.rb
@@ -11,18 +11,10 @@ namespace :cars do # rake cars:delete_cars
     CarsDelete.new.delete_cars
   end
 
-  desc 'Add a car' # rake cars:add_car
-  task :add_car do
-    cars_db = YamlLoad.new(CARS_PATH).data || []
-    new_car = CarsAdd.new.to_hash
-    cars_db.push(new_car)
-    File.open(CARS_PATH, 'w') { |f| YAML.dump(cars_db, f) }
-  end
-
-  desc 'Add N cars' # rake cars:add_cars cars_number=3
+  desc 'Add N cars' # rake cars:add_cars || cars:add_cars cars_number=3
   task :add_cars do
     cars_db = YamlLoad.new(CARS_PATH).data || []
-    cars_number = ENV['cars_number'].to_i
+    cars_number = ENV['cars_number'].nil? ? 1 : ENV['cars_number'].to_i
     i = 1
     while i <= cars_number
       new_car = CarsAdd.new.to_hash
